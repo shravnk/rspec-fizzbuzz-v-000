fffbe928e68f17d40ed6352f9b2f28c4fafe8976
Brendan O'Dea <brendan.ode4@gmail.com>
2017-12-24 16:00:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(input)
-  if (input % 3 == 0) && (input % 5 == 0)
+def fizzbuzz(bb)
+  if (bb % 3 == 0) && (bb % 5 == 0)
     puts "Fizzbuzz"
-  elsif input % 3 == 0
+  elsif bb % 3 == 0
     puts "Fizz"
-  elsif input % 5 == 0
+  elsif bb % 5 == 0
     puts "Buzz"
   else
   end
